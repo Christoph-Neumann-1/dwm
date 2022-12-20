c1d46d2a7efa271bc0020fa3bddb9297a62a9ef4
Christoph Neumann <christophneumann2005@gmail.com>
2022-12-20 08:05:27 +0100

    add rofi

--- config.def.h
+++ config.def.h
@@ -83,6 +83,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *rofiRun[]={"rofi","-show","run",NULL};
+static const char *rofiLaunch[]={"rofi","-show","drun",NULL};
 
 static const char *upvol[]   = { "amixer", "-D", "pipewire","sset","Master","2.5%+", "unmute",     NULL };
 static const char *downvol[] = { "amixer", "-D", "pipewire","sset","Master","2.5%-", "unmute",     NULL };
@@ -131,6 +133,8 @@ static Key keys[] = {
 	{ 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
 	{ 0,                       XF86XK_AudioMute, spawn, {.v = mutevol } },
 	{ 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
+	{MODKEY|Mod1Mask, XK_r,spawn,{.v=rofiRun}},
+	{MODKEY, XK_r,spawn,{.v=rofiLaunch}},
 };
 
 /* button definitions */
